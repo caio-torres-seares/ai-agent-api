6f2609f2572a146460dcc04e1e25018a2b2f4a91
Caio Torres Seares <caiot.seares@gmail.com>
2025-11-23 18:32:22 -0300

    refactor(api): atualizando endpoint /chat para usar models e service

--- app/api/routers/chat_router.py
+++ app/api/routers/chat_router.py
@@ -1,10 +1,16 @@
 from fastapi import APIRouter
+from app.models.chat_request import ChatRequest
+from app.models.chat_response import ChatResponse
+from app.services.chat_service import process_chat_message
+from fastapi import HTTPException, status
 
 
 router = APIRouter(
     prefix="/chat",
+    tags=["chat"],
 )
 
-@router.post("/")
-async def chat_endpoint(payload: dict):
-    return {"message": "Chat endpoint received the payload", "payload": payload}
+@router.post("/", response_model=ChatResponse)
+async def chat_endpoint(payload: ChatRequest):
+        response = process_chat_message(payload.message)
+        return ChatResponse(response=response)
